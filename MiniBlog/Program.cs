8b0d32fec72bec4c5e1ddda44c19c59761554223
binrui <bmu2@slb.com>
2022-11-02 18:21:12 +0800

    feat: add the uer service layer between the user controller and the user store, article store

--- MiniBlog/Program.cs
+++ MiniBlog/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddSingleton<IArticleStore, ArticleControllerContext>();
 builder.Services.AddSingleton<IUserStore, UserControllerContext>();
 builder.Services.AddSingleton<IArticleService, ArticleService>();
+builder.Services.AddSingleton<IUserService, UserService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
--- MiniBlog/Services/IUserService.cs
+++ MiniBlog/Services/IUserService.cs
@@ -1,6 +1,13 @@
-﻿namespace MiniBlog.Services
+﻿using MiniBlog.Model;
+
+namespace MiniBlog.Services
 {
-    public class IUserService
+    public interface IUserService
     {
+        public User Register(User user);
+        public User Update(User user);
+        public List<User> GetAll();
+        public User GetByName(string name);
+        public User Delete(string name);
     }
 }
