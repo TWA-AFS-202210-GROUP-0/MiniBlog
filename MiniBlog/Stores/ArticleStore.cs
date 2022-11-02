c32ffa566f8b482b8556525bb3bb7f29d2f231a6
Mengyu <mhan8@slb.com>
2022-11-02 15:41:23 +0800

    refactor: xxxx

---
+++ MiniBlog/Stores/ArticleStore.cs
@@ -0,0 +1,35 @@
+namespace MiniBlog.Stores
+{
+    using MiniBlog.Model;
+
+    public class ArticleStore : IArticleStore
+    {
+        private List<Article> articles = new List<Article>();
+
+        public Article Save(Article article)
+        {
+            this.articles.Add(article);
+            return article;
+        }
+
+        public List<Article> GetAll()
+        {
+            return this.articles;
+        }
+
+        public bool Delete(Article articles)
+        {
+            return this.articles.Remove(articles);
+        }
+
+        /// <summary>
+        /// This is for test only, please help resolve!
+        /// </summary>
+        public void Init()
+        {
+            articles = new List<Article>();
+            articles.Add(new Article(null, "Happy new year", "Happy 2021 new year"));
+            articles.Add(new Article(null, "Happy Halloween", "Halloween is coming"));
+        }
+    }
+}
--- MiniBlogTest/Program.cs
+++ MiniBlogTest/Program.cs
@@ -1,7 +1,9 @@
+using MiniBlog.Stores;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddSingleton<IArticleStore, ArticleStoreContext>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
